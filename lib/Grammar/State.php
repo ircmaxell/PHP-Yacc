a8b3b35f8d02fbf3d3d681b55efc9f904cf41c12
Nikita Popov <nikita.ppv@googlemail.com>
2017-10-13 18:43:54 +0200

    Don't use magic in State
    This was even more expensive than anticipated.

--- lib/Grammar/State.php
+++ lib/Grammar/State.php
@@ -7,71 +7,30 @@ use PhpYacc\Lalr\Lr1;
 use PhpYacc\Lalr\Conflict;
 use PhpYacc\Lalr\Reduce;
 
-/**
- * @property Reduce[] $reduce
- * @property Conflict $conflict
- * @property Symbol $through
- * @property Lr1 $items
- * @property int $number
- */
 class State
 {
-
     /** @var State[] */
-    public $shifts = []; // public for indirect array modification
-    protected $_reduce;
-    protected $_conflict;
-    protected $_through;
-    protected $_items;
-    protected $_number;
+    public $shifts = [];
+    /** @var Reduce[] */
+    public $reduce;
+    /** @var Conflict|null */
+    public $conflict;
+    /** @var Symbol */
+    public $through;
+    /** @var Lr1 */
+    public $items;
+    /** @var int */
+    public $number;
 
     public function __construct(Symbol $through, Lr1 $items)
     {
-        $this->_through = $through;
-        $this->_items = $items;
-    }
-
-    public function __get($name)
-    {
-        return $this->{'_'.$name};
-    }
-
-    public function __set($name, $value)
-    {
-        $this->{'set' . $name}($value);
-    }
-
-    public function setConflict(Conflict $conflict)
-    {
-        $this->_conflict = $conflict;
-    }
-
-    public function setReduce(array $reduce)
-    {
-        foreach ($reduce as $r) {
-            assert($r instanceof Reduce);
-        }
-        $this->_reduce = $reduce;
-    }
-
-    public function setThrough(Symbol $through)
-    {
-        $this->_through = $through;
-    }
-
-    public function setItems(Lr1 $items)
-    {
-        $this->_items = $items;
-    }
-
-    public function setNumber(int $number)
-    {
-        $this->_number = $number;
+        $this->through = $through;
+        $this->items = $items;
     }
 
     public function isReduceOnly(): bool
     {
         return empty($this->shifts)
-            && $this->_reduce[0]->symbol->isNilSymbol();
+            && $this->reduce[0]->symbol->isNilSymbol();
     }
 }
